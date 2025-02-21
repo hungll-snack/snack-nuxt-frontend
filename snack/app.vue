f8f1b6538c4eb4dbaebdb50a5b5e226cb82a4ab8
ajeseung <ajeseung@naver.com>
2025-02-21 14:16:45 +0900

    feat:[손승일] front 기본 페이지 구성

--- snack/app.vue
+++ snack/app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-container>
+    <v-app>
+      <NavigationBar/>
+      <v-main>
+        <NuxtPage/>
+      </v-main>
+    </v-app>
+  </v-container>
 </template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
--- snack/nuxt.config.ts
+++ snack/nuxt.config.ts
@@ -1,5 +1,62 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config"
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  extends: [
+    './home/nuxt.config.ts',
+    './account/nuxt.config.ts',
+  ],
+
+  css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.min.css',
+    'quill/dist/quill.snow.css',
+  ],
+
+  build: {
+    transpile: [
+      'vuetify',
+      'vue-toastification',
+      // "@vueup/vue-quill", "quill-delta"
+    ]
+  },
+
+  vite: {
+    ssr: {
+      noExternal: [
+        'vuetify',
+      ],
+    },
+  },
+
+  components: [
+    {
+      path: '~/navigationBar', extensions: ['vue'],
+    },
+  ],
+
+  modules: [
+    'vuetify-nuxt-module',
+    '@pinia/nuxt', 
+    '~/home/index.ts',
+    '~/account/index.ts',
+  ],
+
+  imports: {
+    dirs: ['./stores']
+  },
+
+  runtimeConfig: {
+    public: {
+      MAIN_API_URL: process.env.VUE_APP_BASE_URL,
+      AI_BASE_URL: process.env.VUE_APP_AI_BASE_URL,
+      TOSS_CLIENT_KEY : process.env.TOSS_CLIENT_KEY,
+      TOSS_SECRET_KEY : process.env.TOSS_SECRET_KEY,
+      AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+      AWS_REGION: process.env.AWS_REGION,
+      AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+    }
+  },
 })
