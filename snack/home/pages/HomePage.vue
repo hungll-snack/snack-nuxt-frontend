aefd507ca32f7a07e3d02b74d1db4be524ed9383
ajeseung <ajeseung@naver.com>
2025-03-10 15:02:05 +0900

    feat:[손승일] kakao map 추가

--- snack/home/pages/HomePage.vue
+++ snack/home/pages/HomePage.vue
@@ -1,17 +1,9 @@
 <template>
     <v-container fluid class="pa-4">
-        <!-- 배경 이미지와 텍스트를 포함한 Hero 섹션 -->
+        <!-- Kakao Map을 표시할 Hero 섹션 -->
         <v-row>
-            <v-col 
-                cols="12" 
-                style="
-                    align-content: center;
-                    height: 600px; 
-                    text-align: center;
-                    background-color: aqua;
-                "
-                >
-              홈페이지 (추후 작업)
+            <v-col cols="12">
+                <div id="map" style="width: 100%; height: 600px;"></div>
             </v-col>
         </v-row>
 
@@ -25,12 +17,71 @@
     </v-container>
 </template>
 
-<script>
-export default {
-    name: "HomePage"
-}
+<script setup>
+import { useRuntimeConfig } from '#imports'
+import { ref, onMounted } from 'vue'
+
+
+const config = useRuntimeConfig();
+const restaurants = ref([]);
+
+onMounted(async () => {
+    await loadRestaurants();
+    loadKakaoMap();
+});
+
+const loadRestaurants = async () => {
+    try {
+        const response = await fetch('http://localhost:8000/restaurants/list/');
+        restaurants.value = await response.json();
+    } catch (error) {
+        console.error("API 로딩 실패:", error);
+    }
+};
+
+const loadKakaoMap = () => {
+    const KAKAO_API_KEY = config.public.KAKAO_JAVASCRIPT_APP_KEY;
+    if (window.kakao && window.kakao.maps) {
+        initMap();
+    } else {
+        const script = document.createElement("script");
+        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&autoload=false`;
+        script.onload = () => {
+            kakao.maps.load(initMap);
+        };
+        document.head.appendChild(script);
+    }
+};
+
+const initMap = () => {
+    const container = document.getElementById("map");
+    if (!container) return;
+
+    const map = new kakao.maps.Map(container, {
+        center: new kakao.maps.LatLng(37.496486063, 127.028361548),
+        level: 3,
+    });
+
+    addMarkers(map);
+};
+
+const addMarkers = (map) => {
+    restaurants.value.forEach(({ name, latitude, longitude, address }) => {
+        const marker = new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(latitude, longitude),
+            map: map
+        });
+
+        const infoWindow = new kakao.maps.InfoWindow({
+            content: `<div style="padding:5px;font-size:14px;">${name}<br>${address}</div>`
+        });
+
+        kakao.maps.event.addListener(marker, "click", () => {
+            infoWindow.open(map, marker);
+        });
+    });
+};
 </script>
 
 <style scoped>
-
 </style>
