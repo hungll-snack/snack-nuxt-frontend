712b26b7fd30c6e4247ace5fb266c6a7d2951ad0
Hyeonseo2083 <122842851+jungs0914@users.noreply.github.com>
2025-04-10 19:34:58 +0900

    fix: [정현서] 대댓글 기능 및 좋아요 버틈 등 기능 추가 [SNF-10]

--- snack/comment/stores/CommentType.ts
+++ snack/comment/stores/CommentType.ts
@@ -1,15 +1,20 @@
 export interface Comment {
-    comment_id: number;
-    content: string;
-    author_nickname: string;
-    created_at: string;
-    is_deleted?: boolean;
-    is_author?: boolean;
-  }
-  
-  export interface CommentState {
-    comments: Comment[];
-    isLoading: boolean;
-    isSuccess: boolean;
-    errorMessage: string | null;
-  }
+  comment_id: number;
+  content: string;
+  author_nickname: string;
+  created_at: string;
+  author_account_id?: number;
+  is_deleted?: boolean;
+  is_author?: boolean;
+  parent_id?: number;
+  children?: Comment[]; // 트리 구조 재귀적 렌더링
+  is_liked?: boolean;
+  like_count?: number;
+}
+
+export interface CommentState {
+  comments: Comment[];
+  isLoading: boolean;
+  isSuccess: boolean;
+  errorMessage: string | null;
+}
